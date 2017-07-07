b3e06cf1d478752047acbd4bea25c07f20c73d83
ADIKALON <xadikalonx@gmail.com>
2017-07-07 15:35:32 +0300

    reastore auth

--- database/migrations/2017_07_03_083812_create_users_table.php
+++ database/migrations/2017_07_03_083812_create_users_table.php
@@ -14,15 +14,14 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function (Blueprint $table) {
 			$table->increments('id')->lenght(10)->unsigned();
 			$table->integer('ident')->lenght(10)->unsigned()->nullable()->default(NULL);
-			$table->string('login', 255)->nullable()->default(NULL);
+			$table->string('login', 255)->unique()->nullable()->default(NULL);
 			$table->string('pass', 255)->nullable()->default(NULL);
-			$table->string('mail', 255)->nullable()->default(NULL);
+			$table->string('mail', 255)->unique()->nullable()->default(NULL);
 			$table->decimal('refback', 5, 2)->unsigned()->default(0);
 			$table->decimal('balance', 10, 2)->unsigned()->default(0);
 			$table->integer('date')->lenght(10)->unsigned()->nullable()->default(NULL);
 			$table->string('ip', 15)->nullable()->default(NULL);
 			$table->tinyInteger('role')->lenght(1)->unsigned()->nullable()->default(NULL);
-			//$table->string('auth', 255)->nullable()->default(NULL);
 			$table->string('hash', 255)->nullable()->default(NULL);
 			$table->rememberToken();
 		});
